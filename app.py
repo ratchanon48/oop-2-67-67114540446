c24e68beb6fab6466ea525ccf0b8f5e5eab8fc7a
ratchanon48 <ratchanon.ru.67@ubu.ac.th>
2025-01-24 14:04:02 +0700

    สร้างคลาสแรกบน Github repo 555

---
+++ app.py
@@ -0,0 +1,12 @@
+class student(object):
+  student_cont = 0
+
+def __new__(self):
+  print('student.__new__.')
+
+def __niit__(self):
+  print('stodent.__init__')
+  self.agp = 4.00
+
+def Student_hard(self):
+  print('sir, yes sir.')
